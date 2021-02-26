657a0f99c0f4418f8bc0774ee295afb0b034c44e
Dfaw17 <dfawwaz17@gmail.com>
2021-02-26 21:33:44 +0700

    test

--- logic1.py
+++ logic1.py
@@ -6,4 +6,4 @@ response = requests.get("https://airportgap.dev-tester.com/api/airports")
 
 assert_that(response.status_code).is_equal_to(200)
 assert len(response.json().get("data")) > 1
-
+print("faw")
