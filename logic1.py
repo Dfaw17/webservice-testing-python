fcec432cd5ccd2e5f4d723da7fb6163c1d4c8ba2
Dfaw17 <dfawwaz17@gmail.com>
2021-02-26 23:14:03 +0700

    menambahkan print maulana

--- logic1.py
+++ logic1.py
@@ -7,3 +7,4 @@ response = requests.get("https://airportgap.dev-tester.com/api/airports")
 assert_that(response.status_code).is_equal_to(200)
 assert len(response.json().get("data")) > 1
 print("faw")
+print("maulana")
