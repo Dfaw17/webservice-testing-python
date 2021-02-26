7c1658d9952927c00c255d23bd0808d949580aed
Dfaw17 <dfawwaz17@gmail.com>
2021-02-26 21:24:36 +0700

    mengubah logic sample

--- logic1.py
+++ logic1.py
@@ -1,17 +1,9 @@
 import requests
-import json
 from assertpy import assert_that
-a = "HGU"
-b = "My Third Fav Airport"
 
-fav = {
-    "airport_id": a,
-    "note": b
-}
 
-response = requests.post("https://airportgap.dev-tester.com/api/favorites", data=fav, headers = {'Authorization':'Bearer token=bd2Y31TjuZdjE4YXgaHvvYHi'})
-data = response.json().get('errors')[0]['detail']
+response = requests.get("https://airportgap.dev-tester.com/api/airports")
 
-assert response.status_code == 422
-assert data == "This airport is already in your favorites"
+assert_that(response.status_code).is_equal_to(200)
+assert len(response.json().get("data")) > 1
 
